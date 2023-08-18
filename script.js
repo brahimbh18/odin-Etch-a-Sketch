5e9db182095906bd520582bb4301ef01f39a4e78
brahimbh18 <102485344+brahimbh18@users.noreply.github.com>
2023-08-19 00:14:55 +0100

    made the grid respond to the size input and the color too

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 const grid = document.querySelector('.grid');
 const GRID_DIMENTIONS = 600;
 
-var l = 3;
-n = (GRID_DIMENTIONS / l) - 2.67;
-for (i = 0; i < l*l; i++) {
-    var square = document.createElement("div");
-    square.classList.add('square');
-    square.style.width = `${n}px`;
-    square.style.width = `${n}px`;
-    square.addEventListener('mouseover',() => square.classList.add('hovered'));
-    grid.appendChild(square);
+
+
+function loadPage() {
+    grid.innerHTML = "";
+    var l = document.querySelector("#size").value;
+    var color = document.querySelector("#color").value;
+    
+    makeGrid(l, color);
 }
+
+
+function makeGrid(l, color) {
+    
+    document.querySelector(".size").textContent = l;
+
+    n = (GRID_DIMENTIONS / l) - 2;
+    for (i = 1; i < l*l +1; i++) {
+        var square = document.createElement("div");
+        square.classList.add('square');
+        square.setAttribute("id", i);
+        
+        square.style.width = `${n}px`;
+        square.style.width = `${n}px`;
+        grid.appendChild(square);
+        changeColor(square, color);
+       
+    }
+
+}
+function changeColor(square, color) {
+    
+    square.addEventListener('mouseover', () => square.style.backgroundColor = color);
+}
+
+
+function click() {
+    var l = document.querySelector("#size").value;
+    var color = document.querySelector("#color").value;
+    
+    makeGrid(l, color);
+}
+
+loadPage();
